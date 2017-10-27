2377b2030ab67451c4d64873763b2e5c905073a8
madisettyjayaprakash <mjayaprakash21@gmail.com>
2017-10-27 16:50:04 +0530

    commit1

---
+++ ex.pl
@@ -0,0 +1,22 @@
+
+print "enter 1st string\n";
+$dna1 = <STDIN>;
+chomp $dna1;
+print "enter 2 dna\n";
+@dna2
+
+
+for($i=0;$i<3;$i=$i+1){
+
+@dna2[i]=<STDIN>;
+chomp $dna2[i];
+}
+
+
+chomp $dna2;
+#if($dna1 eq $dna2){
+# print "YEs\n";
+ #}
+#else{
+ #print "No\n";
+ #}
